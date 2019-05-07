39b980ea645a82e540b37559bc31d2b57459528d
manojpanta <manojpanta95@gmail.com>
2019-05-06 18:02:49 -0600

    Added food routes, added food router to app.js, installed testing packages
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var foodsRouter = require('./routes/api/v1/foods');
 
 var app = express();
 
@@ -14,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/v1/foods', foodsRouter);
 
 module.exports = app;
---
+++ routes/api/v1/foods.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+// var Food = require('../../../models').Food;
+// var pry = require('pryjs');
+
+
+
+module.exports = router;
