2c55942ad9b55cf156a246084045c6a03a7505ef
Scott Thomas <smthom05@gmail.com>
2019-05-07 07:38:47 -0600

    Created test for getting a specific food endpoint, created seeds file for foods, created actual endpoint.

---
+++ app.spec.js
@@ -0,0 +1,21 @@
+var shell = require('shelljs');
+var request = require('supertest');
+var app = require('./app');
+var pry = require('pryjs')
+
+describe('Foods API', () => {
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+    shell.exec('npx sequelize db:create')
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+
+  describe('Test /api/v1/foods/1', () => {
+    test('should return status 200 and specific food object', () => {
+      return request(app).get('/api/v1/foods/1').then(response => {
+        expect(response.status).toBe(200)
+      })
+    })
+  })
+})
