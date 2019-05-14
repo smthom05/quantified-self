1b52010fa9995e3a17a477042f9292797405af87
Scott Thomas <smthom05@gmail.com>
2019-05-13 18:38:46 -0600

    Add Tests for getting all meals for a user, Add functionality for getting all meals for a user, Add name to users table in DB, Add name to users create endpoint
    Co-authored-by: Manoj Panta <34468256+manojpanta@users.noreply.github.com>

--- seeders/20190509235353-user-seed.js
+++ seeders/20190509235353-user-seed.js
@@ -3,18 +3,21 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
+      name: 'Scott',
       email: 'email@email1.com',
       passwordDigest: "password",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
+      name: 'Manoj',
       email: 'email@email2.com',
       passwordDigest: "password",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
+      name: 'April',
       email: 'email@email3.com',
       passwordDigest: "password",
       createdAt: new Date(),
